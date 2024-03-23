da985670b1633f88a1e9703be83d2caf4da97db0
Andrii <andrii.kondratovich@gmail.com>
2024-03-23 16:07:47 +0200

    add: userInteraction

---
+++ userInteraction/js/index.js
@@ -0,0 +1,21 @@
+const message = 'JavaScript is awesome!';
+console.log(message); // JavaScript is awesome!
+
+const username = 'Mango';
+console.log('Username is ', username); // Username is Mango
+
+const message2 = 'JavaScript is awesome!';
+alert(message2);
+
+const isComing = confirm('Please confirm hotel reservation');
+console.log(isComing);
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt('Please enter desired hotel name');
+console.log(hotelName);
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName2 = prompt('Please enter desired hotel name');
+console.log(hotelName2);
