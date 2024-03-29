8c32ae6b2bb56776b13b64bbd8dce3cc23ad6e86
Andrii <andrii.kondratovich@gmail.com>
2024-03-29 14:32:14 +0200

    add: reduce

--- arrays/js/index.js
+++ arrays/js/index.js
@@ -1,165 +1,49 @@
-// ^Доступ до елементів
-const clients = ["Mango", "Poly", "Ajax"];
-// Зазначаючи в дужках індекс елемента, ми отримуємо його значення
-console.log(clients[0]); // Mango
-console.log(clients[1]); // Poly
-console.log(clients[2]); // Ajax
+// ~ Просунутий reduce
 
-// Перевизначення
-clients[0] = "Kiwi";
-clients[1] = "Pango";
-console.log(clients); // ["Kiwi", "Pango", "Ajax"]
-
-// Довжина масиву
-console.log(clients.length); // 3
-
-// Індекс останнього елемента
-const lastElementIndex = clients.length - 1;
-console.log(lastElementIndex); // 2
-console.log(clients[lastElementIndex]); // "Ajax"
-
-// ^Ітерація по масиву
-// ~Цикл for
-for (let i = 0; i < clients.length; i += 1) {
-  // Дозволяє змінювати елементи масиву, бо в client[i] лежить посилання на клітинку пам'яті
-  // Дозволяє отримати доступ до індексу елементу
-  console.log(clients[i]);
-}
-
-// ~Цикл for...of
-// Не дозволяє отримати доступ до індексу
-// Не дозволяє змінювати елементи масиву, бо в client лежить значення, а не посилання на клітинку пам'яті, як у випадку з for
-// Тобто під капотом відбувається ось це: client = client[i]
-for (const client of clients) {
-  console.log(client);
-}
-
-const string = "javascript";
-for (const character of string) {
-  console.log(character);
-}
-
-// ~Метод includes()
-// під капотом робить перебір масиву і повертає true чи false по умові.
-const loginToFind = "Pango";
-const messageOfIncludes = clients.includes(loginToFind)
-  ? `Користувач ${loginToFind} знайдений`
-  : `Користувач ${loginToFind} не знайдений`;
-console.log("messageOfIncludes:::", messageOfIncludes);
-
-// ~Оператори break і continue
-// break - вихід з циклу (перериває його і виходить)
-// continue - перервати поточну ітерацію циклу.
-
-// ^Методи масиву
-// ~Методи split() і join()
-// Не змінює масив, а повертає новий (змінений).
-const userName = "Mango";
-console.log(userName.split("")); // ["M", "a", "n", "g", "o"]
-const message = "JavaScript - це цікаво";
-console.log(message.split(" ")); // ["JavaScript", "-", "це", "цікаво"]
-
-const words = ["JavaScript", "це", "цікаво"];
-console.log(words.join("")); // "JavaScriptцецікаво"
-console.log(words.join(" ")); // "JavaScript це цікаво"
-console.log(words.join("-")); // "JavaScript-це-цікаво"
-
-// ~Метод indexOf()
-// indexOf(value) повертає перший індекс, в якому елемент зі значенням value був знайдений в масиві, або число -1, якщо такий елемент відсутній.
-const clients4 = ["Mango", "Ajax", "Poly", "Kiwi"];
-console.log(clients4.indexOf("Poly")); // 2
-console.log(clients4.indexOf("Monkong")); // -1
-
-// ~Методи push() і pop() nf unshift і shift()
-// Додають або видаляють крайні елементи масиву.
-// push() і pop() - в кінці масиву
-// unshift і shift() - на початку масиву
-const numbers = [];
-numbers.push(1);
-console.log(numbers); // [1]
-numbers.push(2);
-console.log(numbers); // [1, 2]
-
-const fullNumbers = [1, 2, 3, 4, 5];
-console.log(fullNumbers.pop()); //  5
-console.log(fullNumbers); // [1, 2, 3, 4]
-console.log(fullNumbers.pop()); //  4
-console.log(fullNumbers); // [1, 2, 3]
-
-// ~Метод slice()
-// slice(begin, end) повертає новий масив, що містить копію частини вихідного масиву, не змінюючи його.
-// Копія створюється з begin і до end (але не включно). begin і end - індекси вихідного масиву.
-const clients5 = ["Mango", "Ajax", "Poly", "Kiwi"];
-console.log("clients5:::", clients5);
-console.log(clients5.slice(1, 3)); // ["Ajax", "Poly"]
-
-// Якщо begin і end не зазначені, буде створена повна копія вихідного масиву.
-console.log(clients5.slice()); // ["Mango", Ajax", "Poly", "Kiwi"]
-
-// Якщо не зазначено end, копіювання буде зі start і до кінця вихідного масиву.
-console.log(clients5.slice(1)); // ["Ajax", "Poly", "Kiwi"]
-console.log(clients5.slice(2)); // ["Poly", "Kiwi"]
-
-// Якщо значення start від'ємне, а end не зазначено - будуть скопійовані останні start елементи
-console.log(clients5.slice(-2)); // ["Poly", "Kiwi"]
-
-// ~Метод splice() - швейцарський ніж
-// splice(position, num, new_element_1, new_element_2, ...)
-// Змінює вихідний масив і повертає масив, що містить видалені елементи
-
-// &Видалення - splice(position, num)
-// position - індекс з якого почати
-// num - скільки елементів видаляти
-const scores = [1, 2, 3, 4, 5];
-const deletedScores = scores.splice(0, 3); // Видалить три елементи масиву, починаючи з першого (індекс 0)
-console.log(scores); // [4, 5]
-console.log(deletedScores); // [1, 2, 3]
-
-// &Додавання - splice(position, 0, new_element_1, new_element_2, ...)
-// position - індекс з якого почати
-// 0 - скільки елементів видаляти
-// new_element_1, new_element_2, - нові елементи, що треба додати
-const colors1 = ["red", "green", "blue"];
-colors1.splice(2, 0, "purple");
-console.log(colors1); // ["red", "green", "purple", "blue"]
-
-const colors2 = ["red", "green", "blue"];
-colors2.splice(1, 0, "yellow", "pink");
-console.log(colors2); // ["red", "yellow", "pink", "green", "blue"]
-
-// &Заміна - splice(position, num, new_element_1, new_element_2, ...)
-const languages = ["C", "C++", "Java", "JavaScript"];
-languages.splice(1, 1, "Python"); // Замінить елемент з індексом 1 на новий
-console.log(languages); // ["C", "Python", "Java", "JavaScript"]
-
-languages.splice(2, 1, "C#", "Swift", "Go"); // Замінить один елемент (з індексом 2) на декілька
-console.log(languages); // ["C", "Python", "C#", "Swift", "Go", "JavaScript"]
-
-// ~Метод concat()
-// Об'єднує два або більше масивів в один.
-// Не змінює вихідний масив, повертає новий.
-// Порядок аргументів методу впливає на порядок елементів нового масиву.
-const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
-const newClients = ["Monkong", "Singu"];
-
-const allClientsWithOldFirst = oldClients.concat(newClients);
-console.log(allClientsWithOldFirst); // ["Mango", "Ajax", "Poly", "Kiwi", "Monkong", "Singu"]
-
-const allClientsWithNewFirst = newClients.concat(oldClients);
-console.log(allClientsWithNewFirst); // ["Monkong", "Singu", "Mango", "Ajax", "Poly", "Kiwi"]
-
-console.table(oldClients); // ["Mango", "Ajax", "Poly", "Kiwi"]
-console.log(newClients); // ["Monkong", "Singu"]
-
-const moreArrays = newClients.concat(oldClients, colors2, clients5, [1, 2, 3], [90, 70]);
-console.log("moreArrays::::::::::::::::::::", moreArrays);
-
-// ^Багатомірний масив
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+// порахувати кількість унікальних тегів в масиві
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs", "Oracle"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
 ];
-console.log(matrix[0][0]); // 1
-console.log(matrix[1][2]); // 6
-console.log(matrix[2][2]); // 9
+
+// порахувати суму усіх лайків
+const countLikes = (tweets) => {
+  return tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+};
+console.log("countLikes(tweets)", countLikes(tweets)); // 32
+
+// зберемо в масив усі теги, які зустрічаються в постах.
+const getTags = (tweets) =>
+  tweets.reduce((allTags, tweet) => {
+    allTags.push(...tweet.tags);
+
+    return allTags;
+  }, []);
+
+const tags = getTags(tweets);
+console.log("tags:::", tags);
+
+// Винесемо callback-функцію окремо, а в reducе передамо посилання на неї.
+// Це стандартна практика, якщо callback-функція досить велика.
+
+// Якщо в об'єкті-акумуляторі acc відсутня своя властивість з ключем tag,
+// то створюємо її і записуємо їй значення 0.
+// В іншому випадку збільшуємо значення на 1.
+const getTagStats = (acc, tag) => {
+  if (!acc.hasOwnProperty(tag)) {
+    acc[tag] = 0;
+  }
+
+  acc[tag] += 1;
+
+  return acc;
+};
+
+// Початкове значення акумулятора - це порожній об'єкт {}
+const countTags = (tags) => tags.reduce(getTagStats, {});
+
+const tagCount = countTags(tags);
+console.log("tagCount:::", tagCount);
